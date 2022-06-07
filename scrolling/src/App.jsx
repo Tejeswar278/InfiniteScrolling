2f4be7f1bc5d439104833e52dfe77bbd564b2cf4
Muddada Tejeswara Rao <lsandptlove143@gmail.com>
2022-06-07 08:39:18 +0530

    scrolling is completed

--- scrolling/src/App.jsx
+++ scrolling/src/App.jsx
@@ -1,13 +1,60 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import logo from './logo.svg'
+import axios from "axios"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let offset = 0;
+  const [count, setCount] = useState([])
+
+  const loadmoredata = () => {
+    const newcount = [];
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${offset}`)
+      .then(({data}) => {
+        
+        data.results.forEach((e) => newcount.push(e.name));
+        setCount((count) => [...count, newcount])
+      });
+      offset += 10;
+  }
+
+  const handleScroll = (ele) => {
+    // console.log(ele.target.documentElement.scrollTop)
+    // console.log(ele.target.documentElement.scrollHeight)
+    // if(window.innerHeight + ele.target.documentElement.scrollTop + 1 >= ele.target.documentElement.scrollHeight){
+    //   loadmoredata();
+    // }
+
+    console.log(ele.target.documentElement.scrollTop);
+    console.log(window.innerHeight);
+    console.log(ele.target.documentElement.scrollHeight);
+    // console.log(
+    //   Math.ceil(e.target.documentElement.scrollTop + window.innerHeight)
+    // );
+    const scrollHeight = ele.target.documentElement.scrollHeight;
+    const currentHeight = Math.ceil(
+      ele.target.documentElement.scrollTop + window.innerHeight
+    );
+    if (currentHeight + 1 >= scrollHeight) {
+      loadmoredata();
+    }
+
+  }
+
+  useEffect(() => {
+    loadmoredata();
+    window.addEventListener("scroll", handleScroll)
+  },[])
+  
+
+  
 
   return (
     <div className="App">
-      <div>Masai Student {count}</div>
+      {count.map((e,i) => {
+        return <div key={i} className="container"><div>{i+1}.</div><div>{e}</div><br/></div>
+      })}
     </div>
   )
 }
